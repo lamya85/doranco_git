0dbb30cfae2c8f2864500fd5715c207d222296a7
Lamya AGZOUT <agzout2002@gmx.fr>
2021-07-12 13:57:55 +0200

    nouvelle modification

---
+++ connexion.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Début de la page connexion.php</p>
+
+    <p>Suite de la page</p>
+
+    <p>fin de la page</p>
+
+</body>
+</html>
